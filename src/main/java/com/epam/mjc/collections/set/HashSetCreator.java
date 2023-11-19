8cf5f69970b0dc606ddb1194627257774ad0cedd
8ar7ar <5ar7ar@mail.ru>
2023-11-19 19:07:12 +0500

    stage1-module4-collections-task2-set "1st attempt---locally tested"

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,23 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (Integer intNum: sourceList){
+            int num = intNum;
+
+            if (num%2==0) {
+                while (num%2==0) {
+                    result.add(num);
+                    num /= 2;
+                }
+                result.add(num);
+            }
+            else{
+                result.add(num);
+                result.add(2*num);
+            }
+        }
+        return result;
     }
+
 }
